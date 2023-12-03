721eb11f71e6cf38cbbf7f3b1496a553c93fb69b
= <=>
2023-12-03 16:01:31 +0100

    update

--- api.js
+++ api.js
@@ -1,29 +1,7 @@
-export const fetchPerson = async function fetchRandomUser() {
-    try {
-        const response = await fetch('https://randomuser.me/api');
-        const data = await response.json();
-        return data.results[0];
-    } catch {
-        console.error('Error fetching');
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
     }
-};
-
-export const fetchCurrentTime = async function fetchCurrentTime(location) {
-    const url = `https://world-time-by-api-ninjas.p.rapidapi.com/v1/worldtime?city=${location}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'bb2933d7admsh3a9c922ab33f00cp1ce815jsne81cb4069232',
-            'X-RapidAPI-Host': 'world-time-by-api-ninjas.p.rapidapi.com',
-        },
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-        return result;
-    } catch (error) {
-        console.error('Error fetching current time:', error);
-    }
-};
+  });
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+    base: "https://pablo273520222.github.io/T4---P12---Random-User---Evaluable"
+})
